5f1c402cd10f5814deef52b5621e83b45c26a83d
Daniel Paulino <danielpaulino030@gmail.com>
2017-11-19 04:27:28 +0000

    Initial flow

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const parser = require('./lib/parser');
 const bz2 = require('unbzip2-stream');
 const download = require('./lib/download');
+const parser = require('./lib/parser');
+const saveBook = require('./lib/save-book');
 
 download()
-  // .pipe(bz2())
-  // .pipe(parser())
-  // .on('end', () => console.log('finished'));
+  .pipe(bz2())
+  .pipe(parser())
+  .pipe(saveBook())
+  .on('end', () => console.log('finished'));
